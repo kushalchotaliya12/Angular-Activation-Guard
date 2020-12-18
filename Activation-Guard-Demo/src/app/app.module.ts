ce305fb7126831043e7bfbb409bcdbde8875db5c
Kushal Chotaliya <kushalv.1997@gmail.com>
2020-12-18 02:02:33 -0500

    Initial Commit

---
+++ Activation-Guard-Demo/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { UserInfoFormComponent } from './user-info-form/user-info-form.component';
+import { OrganizationFormComponent } from './organization-form/organization-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserInfoFormComponent,
+    OrganizationFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
