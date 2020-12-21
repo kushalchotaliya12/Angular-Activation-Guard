55590f815bc104db0d2584a32d74c5138ace5beb
Kushal Chotaliya <kushalv.1997@gmail.com>
2020-12-20 23:46:47 -0500

    Add Routing

--- Activation-Guard-Demo/src/app/app-routing.module.ts
+++ Activation-Guard-Demo/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrganizationFormComponent } from './pages/organization-form/organization-form.component';
+import { UserInfoFormComponent } from './pages/user-info-form/user-info-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'userInfo' , component: UserInfoFormComponent},
+  {path: 'organizationForm' , component: OrganizationFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
